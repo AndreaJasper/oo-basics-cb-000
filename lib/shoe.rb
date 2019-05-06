71903af41c0e17e4b17b704b749889b377acf639
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-05-06 14:49:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,32 +7,4 @@ class Shoe
     @brand = brand
   end
 
-  def brand
-    @brand
-  end
-
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(num)
-    @size = num
-  end
-
-  def size
-    @size
-  end
-
-  def material=(material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
-
 end
